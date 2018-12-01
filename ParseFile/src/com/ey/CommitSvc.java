25013e6b7eebb4fa8f8b4748b8feb39bc6d7bb23
bsngupta <bsngupta@users.noreply.github.com>
2018-11-30 22:39:36 -0500

    changed the code to accept folder path as parameter

--- ParseFile/src/com/ey/CommitSvc.java
+++ ParseFile/src/com/ey/CommitSvc.java
@@ -8,19 +8,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CommitSvc {
-    String fPath = "C:\\Users\\bestasu\\Desktop\\";
-    //    String fPath = "D:\\gpb_tools\\apps\\amt-wls-uat\\AMT\\Commit_Temp\\";
     private List<CommitFile> commitRowsList = new ArrayList<CommitFile>();
-    public List<CommitFile> CommitSvc(String fName1, String vName) throws IOException {
-        //String fName1 = "";
-        String fName = fPath+fName1;
+
+    public List<CommitFile> CommitSvc(String fName1, String vName, String fldrpath) throws IOException {
+        String fName = fldrpath + fName1;
         commitRowsList.clear();
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fName), StandardCharsets.UTF_8));
         String line = "";
 
         while ((line = br.readLine()) != null) {
-//            String[] data = line.split("\t");
-            String[] data = (line+"\t").split("\t",-1);
+            String[] data = (line + "\t").split("\t", -1);
             CommitFile cf = new CommitFile();
 
             cf.setManagement(data[0]);
@@ -32,62 +29,62 @@ public class CommitSvc {
             cf.setCurrency(data[6]);
             cf.setGeo(data[7]);
             cf.setAccount(data[8]);
-            if (data[9].length()==0) {
+            if (data[9].length() == 0) {
                 cf.setJul("#MISSING");
             } else {
                 cf.setJul(data[9]);
             }
-            if (data[10].length()==0) {
+            if (data[10].length() == 0) {
                 cf.setAug("#MISSING");
             } else {
                 cf.setAug(data[10]);
             }
-            if (data[11].length()==0) {
+            if (data[11].length() == 0) {
                 cf.setSep("#MISSING");
             } else {
                 cf.setSep(data[11]);
             }
-            if (data[12].length()==0) {
+            if (data[12].length() == 0) {
                 cf.setOct("#MISSING");
             } else {
                 cf.setOct(data[12]);
             }
-            if (data[13].length()==0) {
+            if (data[13].length() == 0) {
                 cf.setNov("#MISSING");
             } else {
                 cf.setNov(data[13]);
             }
-            if (data[14].length()==0) {
+            if (data[14].length() == 0) {
                 cf.setDec("#MISSING");
             } else {
                 cf.setDec(data[14]);
             }
-            if (data[15].length()==0) {
+            if (data[15].length() == 0) {
                 cf.setJan("#MISSING");
             } else {
                 cf.setJan(data[15]);
             }
-            if (data[16].length()==0) {
+            if (data[16].length() == 0) {
                 cf.setFeb("#MISSING");
             } else {
                 cf.setFeb(data[16]);
             }
-            if (data[17].length()==0) {
+            if (data[17].length() == 0) {
                 cf.setMar("#MISSING");
             } else {
                 cf.setMar(data[17]);
             }
-            if (data[18].length()==0) {
+            if (data[18].length() == 0) {
                 cf.setApr("#MISSING");
             } else {
                 cf.setApr(data[18]);
             }
-            if (data[19].length()==0) {
+            if (data[19].length() == 0) {
                 cf.setMay("#MISSING");
             } else {
                 cf.setMay(data[19]);
             }
-            if (data[20].length()==0) {
+            if (data[20].length() == 0) {
                 cf.setJun("#MISSING");
             } else {
                 cf.setJun(data[20]);
@@ -99,23 +96,17 @@ public class CommitSvc {
         return commitRowsList;
     }
 
-    public void fmtFile(String fName1) throws IOException {
-        String amtLoadFile=fPath+fName1;
-//        FileWriter writer = new FileWriter(amtLoadFile);
-//        Writer fstream = null;
-//        BufferedWriter out = null;
+    public void fmtFile(String fName1, String fldrpath) throws IOException {
+        String amtLoadFile = fldrpath + fName1;
         Writer fstream = new OutputStreamWriter(new FileOutputStream(amtLoadFile), StandardCharsets.UTF_8);
 
-
-        fstream.write("\"Account\",\"Jul\",\"Aug\",\"Sep\",\"Oct\",\"Nov\",\"Dec\",\"Jan\",\"Feb\",\"Mar\",\"Apr\",\"May\",\"Jun\",\"Point-Of-View\",\"Data Load Cube Name\""+"\n");
-        for(CommitFile str: commitRowsList.subList(1,commitRowsList.size())) {
-            fstream.write(str.getAccount()+",\""+str.getJul()+"\",\""+str.getAug()+"\",\""+str.getSep()+"\",\""+str.getOct()+"\",\""+
-                    str.getNov()+"\",\""+str.getDec()+"\",\""+str.getJan()+"\",\""+str.getFeb()+"\",\""+str.getMar()+"\",\""+str.getApr()+"\",\""+
-                    str.getMay()+"\",\""+str.getJun()+"\",\"\""+str.getYear()+"\",\""+str.getScenario()+"\",\""+str.getVersion()+"\",\""+
-                    str.getManagement()+"\",\""+str.getServiceline()+"\",\""+str.getRank()+"\",\""+str.getCurrency()+"\"\","+"\""+"PLAN"+"\""+"\n");
+        fstream.write("\"Account\",\"Jul\",\"Aug\",\"Sep\",\"Oct\",\"Nov\",\"Dec\",\"Jan\",\"Feb\",\"Mar\",\"Apr\",\"May\",\"Jun\",\"Point-Of-View\",\"Data Load Cube Name\"" + "\n");
+        for (CommitFile str : commitRowsList.subList(1, commitRowsList.size())) {
+            fstream.write(str.getAccount() + ",\"" + str.getJul() + "\",\"" + str.getAug() + "\",\"" + str.getSep() + "\",\"" + str.getOct() + "\",\"" +
+                    str.getNov() + "\",\"" + str.getDec() + "\",\"" + str.getJan() + "\",\"" + str.getFeb() + "\",\"" + str.getMar() + "\",\"" + str.getApr() + "\",\"" +
+                    str.getMay() + "\",\"" + str.getJun() + "\",\"\"" + str.getYear() + "\",\"" + str.getScenario() + "\",\"" + str.getVersion() + "\",\"" +
+                    str.getManagement() + "\",\"" + str.getServiceline() + "\",\"" + str.getRank() + "\",\"" + str.getCurrency() + "\"\"," + "\"" + "PLAN" + "\"" + "\n");
         }
         fstream.close();
-//        writer.close();
-
     }
 }
--- ParseFile/src/com/ey/Main.java
+++ ParseFile/src/com/ey/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         CommitSvc CS = new CommitSvc();
-        List<CommitFile> rowsList = CS.CommitSvc(args[0],args[1]);
-        CS.fmtFile(args[0]);
+        List<CommitFile> rowsList = CS.CommitSvc(args[0], args[1], args[2]);
+        CS.fmtFile(args[0], args[2]);
     }
 }
 
